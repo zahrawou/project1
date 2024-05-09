e72372b86c220ee91c36502a2d93367672b857b3
ferialZ <167868795+ferialZ@users.noreply.github.com>
2024-05-09 09:49:05 +0200

    Add files via upload

---
+++ HomeJs.js
@@ -0,0 +1,16 @@
+function openFunction(){
+    document.getElementById('ul1').style.width = '50%';
+ document.querySelector('.profile').style.display='none';
+    // document.querySelector(".profile").style.;
+
+    
+
+
+}
+
+
+function closeFunction(){
+    
+    document.getElementById('ul1').style.width = '0px';
+    
+}
